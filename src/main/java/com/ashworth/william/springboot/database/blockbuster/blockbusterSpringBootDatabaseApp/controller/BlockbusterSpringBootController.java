d7f3cd18d8a2e48074217c7769d5f9f156b104a6
washworth01 <washworth01@gmail.com>
2019-01-17 17:36:50 +0000

    likely finished

--- src/main/java/com/ashworth/william/springboot/database/blockbuster/blockbusterSpringBootDatabaseApp/controller/BlockbusterSpringBootController.java
+++ src/main/java/com/ashworth/william/springboot/database/blockbuster/blockbusterSpringBootDatabaseApp/controller/BlockbusterSpringBootController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.ashworth.william.springboot.database.blockbuster.blockbusterSpringBoo
 import com.ashworth.william.springboot.database.blockbuster.blockbusterSpringBootDatabaseApp.model.BlockbusterSpringBootModel;
 import com.ashworth.william.springboot.database.blockbuster.blockbusterSpringBootDatabaseApp.repository.BlockbusterSpringBootRepository;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class BlockbusterSpringBootController 
